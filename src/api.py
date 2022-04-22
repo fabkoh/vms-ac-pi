9a33696343cc6e7e91e76eac51ee1a6689316c79
ong-wei-hong <weihong.ong@outlook.com>
2022-04-22 14:39:53 +0800

    POC: json_readers_test.py proves that containers can be created that track variabled on other files

--- src/api.py
+++ src/api.py
@@ -8,10 +8,10 @@ app = flask.Flask(__name__)
 app.config['DEBUG'] = (os.environ.get('FLASK_ENV', 'PRODUCTION') == 'DEVELOPMENT')
 
 # # uncomment to test
-# @app.route('/test/change-json')
-# def change_test():
-#     TestJson.write({'a': randint(0, 100)})
-#     return flask.Response({}, 200, {'Content-type': 'application/json'})
+@app.route('/test/change-json')
+def change_test():
+    TestJson.write({'a': randint(0, 100)})
+    return flask.Response({}, 200, {'Content-type': 'application/json'})
 
 def main():
     app.run(host='0.0.0.0', port=5000, debug=app.config['DEBUG'])
