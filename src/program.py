c5b13f639db7836735b7f6f4d5b0ec97fc04a735
bicky <bickybong@gmail.com>
2023-02-03 10:56:01 +0800

    testing auth methods

--- src/program.py
+++ src/program.py
@@ -1,7 +1,7 @@
 import GPIOconfig
 import events
 import threading
-import pigpio 
+import pigpio
 import eventsMod
 import healthcheck
 
@@ -19,20 +19,28 @@ E1_OUT = None
 E2_IN = None
 E2_OUT = None
 
+
 def mag_and_button():
-    cb1 = GPIOconfig.pi.callback(events.E1_Mag, pigpio.RISING_EDGE, events.mag_detects_rising)
-    cb2 = GPIOconfig.pi.callback(events.E1_Mag, pigpio.FALLING_EDGE, events.mag_detects_falling)
-    cb3 = GPIOconfig.pi.callback(events.E2_Mag, pigpio.RISING_EDGE, events.mag_detects_rising)
-    cb4 = GPIOconfig.pi.callback(events.E2_Mag, pigpio.FALLING_EDGE, events.mag_detects_falling)
-    cb5 = GPIOconfig.pi.callback(events.E1_Button, pigpio.FALLING_EDGE, events.button_detects_change)
-    cb6 = GPIOconfig.pi.callback(events.E2_Button, pigpio.FALLING_EDGE, events.button_detects_change)
+    cb1 = GPIOconfig.pi.callback(
+        events.E1_Mag, pigpio.RISING_EDGE, events.mag_detects_rising)
+    cb2 = GPIOconfig.pi.callback(
+        events.E1_Mag, pigpio.FALLING_EDGE, events.mag_detects_falling)
+    cb3 = GPIOconfig.pi.callback(
+        events.E2_Mag, pigpio.RISING_EDGE, events.mag_detects_rising)
+    cb4 = GPIOconfig.pi.callback(
+        events.E2_Mag, pigpio.FALLING_EDGE, events.mag_detects_falling)
+    cb5 = GPIOconfig.pi.callback(
+        events.E1_Button, pigpio.FALLING_EDGE, events.button_detects_change)
+    cb6 = GPIOconfig.pi.callback(
+        events.E2_Button, pigpio.FALLING_EDGE, events.button_detects_change)
+
 
 def check_events_for(entrance):
     if entrance.split("_")[0] == "E1":
         entrancename = events.E1
     if entrance.split("_")[0] == "E2":
         entrancename = events.E2
-    
+
     if entrance == "E1_IN":
         credentials = events.credentials_E1_IN
         pinsvalue = events.pinsvalue_E1_IN
@@ -72,21 +80,21 @@ def check_events_for(entrance):
         CRED_TIMEOUT = events.CRED_TIMEOUT_E2
         MAG_TIMEOUT = events.MAG_TIMEOUT_E2
         BUZZER_TIMEOUT = events.BUZZER_TIMEOUT_E2
-    
-    if timeout_cred.status(): 
-            if timeout_cred.check(CRED_TIMEOUT):
-                timeout_cred.stop()
-                credentials.clear()
-                pinsvalue.clear()
-    
+
+    if timeout_cred.status():
+        if timeout_cred.check(CRED_TIMEOUT):
+            timeout_cred.stop()
+            credentials.clear()
+            pinsvalue.clear()
+
     if timeout_buzzer.status():
         if timeout_buzzer.check(BUZZER_TIMEOUT):
             print("email")
-        
+
     if timeout_mag.status():
         if timeout_mag.check(MAG_TIMEOUT):
             GPIOconfig.activate_buzz_led(entrance[:2])
-            
+
             if not timeout_buzzer.status():
                 timeout_buzzer.start()
                 print("Buzzer started buzzing")
@@ -102,9 +110,9 @@ def check_events_for(entrance):
     events.time.sleep(0.1)
 
 
-#E1_is_active/E2_is_active
+# E1_is_active/E2_is_active
 def check_entrance_E1():
-    if not events.E1_is_active: 
+    if not events.E1_is_active:
         events.relay.unlock_entrance_one()
     else:
         events.relay.lock_entrance_one()
@@ -113,9 +121,11 @@ def check_entrance_E1():
         else:
             events.relay.lock_entrance_one()
 
-#E1_is_active/E2_is_active
+# E1_is_active/E2_is_active
+
+
 def check_entrance_E2():
-    if not events.E2_is_active: 
+    if not events.E2_is_active:
         events.relay.unlock_entrance_two()
     else:
         events.relay.lock_entrance_two()
@@ -123,7 +133,8 @@ def check_entrance_E2():
             events.relay.unlock_entrance_two()
         else:
             events.relay.lock_entrance_two()
-    
+
+
 def check_events_timer():
     while True:
         check_events_for("E1_IN")
@@ -131,35 +142,43 @@ def check_events_timer():
         check_events_for("E2_IN")
         check_events_for("E2_OUT")
         events.check_entrance_status()
-        
-        #check_entrance_E1()
-        #check_entrance_E2()
-        
+
+        # check_entrance_E1()
+        # check_entrance_E2()
+
+
 def check_gen_pins_and_alarm():
     import eventActionTriggers
     import eventActionTriggerConstants
 
-    def helper(pin,event_trigger):
+    def helper(pin, event_trigger):
         '''
         Args:
             pin: gpio_pin
             event_trigger: input_event_trigger from eventTriggerConstants
         '''
-        def f(gpio,level,tick):
-            if gpio==pin:
-                eventActionTriggers.event_trigger_cb(eventActionTriggerConstants.create_event(event_trigger,eventActionTriggerConstants.BOTH_ENTRANCE))
+        def f(gpio, level, tick):
+            if gpio == pin:
+                eventActionTriggers.event_trigger_cb(eventActionTriggerConstants.create_event(
+                    event_trigger, eventActionTriggerConstants.BOTH_ENTRANCE))
 
         return f
 
     if GPIOconfig.Gen_In_1 != None:
-        cb1 = GPIOconfig.pi.callback(GPIOconfig.Gen_In_1, pigpio.RISING_EDGE, helper(GPIOconfig.Gen_In_1,eventActionTriggerConstants.GEN_IN_1))
+        cb1 = GPIOconfig.pi.callback(GPIOconfig.Gen_In_1, pigpio.RISING_EDGE, helper(
+            GPIOconfig.Gen_In_1, eventActionTriggerConstants.GEN_IN_1))
     if GPIOconfig.Gen_In_2 != None:
-        cb2 = GPIOconfig.pi.callback(GPIOconfig.Gen_In_2,pigpio.RISING_EDGE, helper(GPIOconfig.Gen_In_2,eventActionTriggerConstants.GEN_IN_2))
-    if GPIOconfig.Gen_In_2 != None:
-        cb3 = GPIOconfig.pi.callback(GPIOconfig.Gen_In_3,pigpio.RISING_EDGE,helper(GPIOconfig.Gen_In_3,eventActionTriggerConstants.GEN_IN_3))
-    cb4=GPIOconfig.pi.callback(GPIOconfig.Fire,pigpio.RISING_EDGE,helper(GPIOconfig.Fire,eventActionTriggerConstants.FIRE))
-    
+        cb2 = GPIOconfig.pi.callback(GPIOconfig.Gen_In_2, pigpio.RISING_EDGE, helper(
+            GPIOconfig.Gen_In_2, eventActionTriggerConstants.GEN_IN_2))
+    if GPIOconfig.Gen_In_3 != None:
+        cb3 = GPIOconfig.pi.callback(GPIOconfig.Gen_In_3, pigpio.RISING_EDGE, helper(
+            GPIOconfig.Gen_In_3, eventActionTriggerConstants.GEN_IN_3))
+    cb4 = GPIOconfig.pi.callback(GPIOconfig.Fire, pigpio.RISING_EDGE, helper(
+        GPIOconfig.Fire, eventActionTriggerConstants.FIRE))
+
 # WARNING READ DESCRIPTION
+
+
 def update_config():
     '''call this after calling GPIOconfig.update_config(), events.update_config() and events.update_credOccur()
 WARNING THIS FUNCTION DOES NOT WORK
@@ -174,21 +193,25 @@ it adds an addtion detect_bits call, so multiple detect_bits are called after ca
         E2_IN.cancel()
     if E2_OUT:
         E2_OUT.cancel()
-    
-    E1_IN = GPIOconfig.decoder(GPIOconfig.pi, GPIOconfig.E1_IN_D0, GPIOconfig.E1_IN_D1, events.reader_detects_bits,"E1_IN") 
-    E1_OUT = GPIOconfig.decoder(GPIOconfig.pi, GPIOconfig.E1_OUT_D0, GPIOconfig.E1_OUT_D1, events.reader_detects_bits,"E1_OUT")
 
-    E2_IN = GPIOconfig.decoder(GPIOconfig.pi, GPIOconfig.E2_IN_D0, GPIOconfig.E2_IN_D1, events.reader_detects_bits,"E2_IN") 
-    E2_OUT = GPIOconfig.decoder(GPIOconfig.pi, GPIOconfig.E2_OUT_D0, GPIOconfig.E2_OUT_D1, events.reader_detects_bits,"E2_OUT")
+    E1_IN = GPIOconfig.decoder(GPIOconfig.pi, GPIOconfig.E1_IN_D0,
+                               GPIOconfig.E1_IN_D1, events.reader_detects_bits, "E1_IN")
+    E1_OUT = GPIOconfig.decoder(GPIOconfig.pi, GPIOconfig.E1_OUT_D0,
+                                GPIOconfig.E1_OUT_D1, events.reader_detects_bits, "E1_OUT")
+
+    E2_IN = GPIOconfig.decoder(GPIOconfig.pi, GPIOconfig.E2_IN_D0,
+                               GPIOconfig.E2_IN_D1, events.reader_detects_bits, "E2_IN")
+    E2_OUT = GPIOconfig.decoder(GPIOconfig.pi, GPIOconfig.E2_OUT_D0,
+                                GPIOconfig.E2_OUT_D1, events.reader_detects_bits, "E2_OUT")
 
 
 update_config()
-#update_config()
-#update_config()
+# update_config()
+# update_config()
 
 t1 = threading.Thread(target=check_events_timer)
-t2=threading.Thread(target=mag_and_button)
-t3=threading.Thread(target=check_gen_pins_and_alarm)
+t2 = threading.Thread(target=mag_and_button)
+t3 = threading.Thread(target=check_gen_pins_and_alarm)
 t1.start()
 t2.start()
 t3.start()
