46cb424fe9ae249e9ad0f159a444f709db5f0cb0
bicky <bickybong@gmail.com>
2023-02-03 16:39:51 +0800

    testing gen out

--- src/relay.py
+++ src/relay.py
@@ -147,7 +147,7 @@ def toggleRelayGen(relayPin, activateLevel, activateMilliSeconds):
     sleep(activateMilliSeconds)
     deActivateRelay(relayPin, activateLevel)
     print(f"deactivate {relayPin}")
-    sleep(activateMilliSeconds + 1)
+    sleep(activateMilliSeconds + 10)
     return
 # *** Tests ***
 
