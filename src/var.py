c89ce34828e56500f63cfd4cf793b30f83e03eaf
bicky <bickybong@gmail.com>
2023-03-03 15:19:44 +0800

    testing events

--- src/var.py
+++ src/var.py
@@ -1,3 +1,4 @@
 
-server_url = "http://118.201.255.164:8082"
-# server_url = "http://192.168.1.233:8082"
+server_url = "http://192.168.1.233:8082"
+# public ip
+# server_url = "http://118.201.255.164:8082"
