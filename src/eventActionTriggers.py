5dacdba749e4f23315ed8c7f461fe2d7557c8d09
bicky <bickybong@gmail.com>
2023-03-06 16:10:21 +0800

    testing EMS email/sms

--- src/eventActionTriggers.py
+++ src/eventActionTriggers.py
@@ -43,14 +43,14 @@ def sendEmail_function(event):
     entrance = event.get("entrance", {}).get("entranceId", None)
     print(f"sendEmail to entrance {entrance}")
     url = server_url+'/api/notification/eventsSMTP'
-    event["inputEvents"] = event["inputEvents"][0]
-    event["outputActions"] = event["outputActions"][0]
-    event["triggerSchedule"] = event["triggerSchedule"][0]
-    event["entrance"] = event["entrance"][0]
-    # print(event)
+    # event["inputEvents"] = event["inputEvents"][0]
+    # event["outputActions"] = event["outputActions"][0]
+    # event["triggerSchedule"] = event["triggerSchedule"][0]
+    # event["entrance"] = event["entrance"][0]
+    print(event)
     data = json.dumps(
         event)
-    print(data)
+    # print(data)
     print(f"url is {url}")
 
     try:
