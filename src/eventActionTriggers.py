e60188e0161d891ebe8dde2231f5171b9215ab33
bickybong <bickybong@gmail.com>
2023-03-10 14:40:37 +0800

    email notif dones

--- src/eventActionTriggers.py
+++ src/eventActionTriggers.py
@@ -379,7 +379,7 @@ def check_for_only_timer_based_events():
             if valid:
                 # timer based must have activated
                 activated[event_management_id] = True
-                queue_output(event)
+                # queue_output(event)
 
         flush_output()
         time.sleep(0.1)  # throttle
