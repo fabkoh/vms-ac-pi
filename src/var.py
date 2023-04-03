6cd7ceab62886aa5ab775b1299b299d73ea49f6b
bickybong <bickybong@gmail.com>
2023-04-03 17:23:08 +0800

    change be

--- src/var.py
+++ src/var.py
@@ -1,6 +1,11 @@
 # zephan private ip
 # server_url = "http://192.168.1.233:8082"
-# esther private ip
-server_url = "http://192.168.1.156:8082"
+
 # zephan public ip
 # server_url = "http://118.201.255.164:8082"
+
+# esther private ip
+# server_url = "http://192.168.1.156:8082"
+
+# PI BE server
+server_url = "http://192.168.1.250:8082"
