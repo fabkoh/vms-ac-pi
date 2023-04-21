2dd28601bf0f56f09b098251f64ffb0a87ccf571
bickybong <bickybong@gmail.com>
2023-04-21 14:46:24 +0800

    update pi

--- src/var.py
+++ src/var.py
@@ -5,7 +5,7 @@
 # server_url = "http://118.201.255.164:8082"
 
 # esther private ip
-# server_url = "http://192.168.1.156:8082"
+server_url = "http://192.168.1.156:8082"
 
 # PI BE server
-server_url = "http://192.168.1.250:8082"
+# server_url = "http://192.168.1.9:8082"
