b7962d9a95b632c5c7de9a39cebb39d8263e48eb
bickybong <bickybong@gmail.com>
2023-04-21 14:53:21 +0800

    update pi

--- src/var.py
+++ src/var.py
@@ -1,9 +1,6 @@
 # zephan private ip
 # server_url = "http://192.168.1.233:8082"
 
-# zephan public ip
-# server_url = "http://118.201.255.164:8082"
-
 # esther private ip
 server_url = "http://192.168.1.1:8082"
 
