8454369e2903d62c1153d6503b1a2922e15f776c
junnengsoo <soojunneng01@gmail.com>
2023-05-09 22:14:17 -0700

    Add junneng private IP in var.py

--- src/var.py
+++ src/var.py
@@ -2,7 +2,10 @@
 # server_url = "http://192.168.1.233:8082"
 
 # esther private ip
-server_url = "http://192.168.1.1:8082" 
+# server_url = "http://192.168.1.1:8082" 
 
 # PI BE server
 # server_url = "http://192.168.1.250:8082"
+
+# junneng private ip
+server_url = "http://192.168.1.162:8082"
