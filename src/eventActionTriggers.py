9dbc4af7eff13bfca70f02c16a32c18f88981a32
bickybong <bickybong@gmail.com>
2023-05-12 15:46:58 +0800

    added env

--- src/eventActionTriggers.py
+++ src/eventActionTriggers.py
@@ -61,7 +61,7 @@ def sendEmail_function(event):
         print("sending email")
         headers = {'Content-type': 'application/json'}
         r = requests.post(url, data=data, headers=headers,
-                          verify=False, timeout=0.5)
+                          verify=False, timeout=5)
         print(r)
         print(r.status_code)
 
@@ -99,7 +99,7 @@ def sendSMS_function(event):
     try:
         headers = {'Content-type': 'application/json'}
         r = requests.post(url, data=data, headers=headers,
-                          verify=False, timeout=0.5)
+                          verify=False, timeout=5)
         print(r)
         print(r.status_code)
 
