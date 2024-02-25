43b0ecffe45db2dc0a0076a6036d3ba61033c05a
Yong Ning <yongning.lee@isssecurity.com>
2024-02-25 15:55:27 +0800

    test buzzing and LED

--- src/GPIOconfig.py
+++ src/GPIOconfig.py
@@ -260,7 +260,8 @@ def activate_buzz_led(entrance) :
    # for pin in gpio_pins_bcm:
    #    pi.set_mode(pin, pigpio.OUTPUT)
    #    pi.write(pin, 0)
-
+   pi.write(E1_OUT_Buzz,1)
+   pi.write(E1_IN_Buzz,1)
    pi.write(E1_OUT_Buzz,1)
    pi.write(E1_IN_Buzz,1)
    pi.write(E1_OUT_Led,1)
