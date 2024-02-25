ff1641bc6b667da6e22da40fd48426c4126d223c
Yong Ning <yongning.lee@isssecurity.com>
2024-02-25 15:39:12 +0800

    test buzzing and LED

--- src/GPIOconfig.py
+++ src/GPIOconfig.py
@@ -284,9 +284,9 @@ activate_buzz_led("E1")
 # Function to test a pin
 def test_pin(pin, duration=2):
     print(f"Testing {pin}")
-    pi.write(pins[pin], 1)  # Turn on
+    pi.write(pin, 1)  # Turn on
     time.sleep(duration)
-    pi.write(pins[pin], 0)  # Turn off
+    pi.write(pin, 0)  # Turn off
     print(f"Tested {pin}\n")
 
 gpio_pins_bcm = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
