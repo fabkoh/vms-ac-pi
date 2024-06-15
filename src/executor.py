dde35a7460ea84230b71037389feaed4939b8d50
junnengsoo <soojunneng01@gmail.com>
2024-06-15 16:50:26 +0800

    Change etlas to unicon

--- src/executor.py
+++ src/executor.py
@@ -7,7 +7,7 @@ import logging
 # Setup logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('/home/etlas/ThreadPool.log')
+file_handler = logging.FileHandler('/home/unicon/ThreadPool.log')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
