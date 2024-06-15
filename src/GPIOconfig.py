45761bd8f6a3017a528f90d33139ebd1939b7256
junnengsoo <soojunneng01@gmail.com>
2024-06-15 17:08:24 +0800

    Change E2 pin to 1

--- src/GPIOconfig.py
+++ src/GPIOconfig.py
@@ -407,9 +407,6 @@ def test_buzz():
    print("Testing buzzer")
    pi.write(E1_IN_Buzz, 1)  # Turn on the buzzer connected to E2_IN_Buzz
    pi.write(E1_OUT_Buzz, 1)  # Turn on the buzzer connected to E2_OUT_Buzz
-   time.sleep(10)  # Keep the buzzers on for 10 seconds
-   pi.write(E1_IN_Buzz, 0)  # Turn off the buzzer connected to E2_IN_Buzz
-   pi.write(E1_OUT_Buzz, 0)  # Turn off the buzzer connected to E2_OUT_Buzz
    print("Buzzer test completed")
 
 
