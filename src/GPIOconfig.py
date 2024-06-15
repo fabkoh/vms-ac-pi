ed40ff0bd9b723931de5835ea1a3d84f4569d4a2
junnengsoo <soojunneng01@gmail.com>
2024-06-15 17:10:07 +0800

    Add sleep

--- src/GPIOconfig.py
+++ src/GPIOconfig.py
@@ -407,6 +407,7 @@ def test_buzz():
    print("Testing buzzer")
    pi.write(E1_IN_Buzz, 1)  # Turn on the buzzer connected to E2_IN_Buzz
    pi.write(E1_OUT_Buzz, 1)  # Turn on the buzzer connected to E2_OUT_Buzz
+   time.sleep(5)
    pi.write(E1_IN_Buzz, 0)  # Turn on the buzzer connected to E2_IN_Buzz
    pi.write(E1_OUT_Buzz, 0)  # Turn on the buzzer connected to E2_OUT_Buzz
    print("Buzzer test completed")
