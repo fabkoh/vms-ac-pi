c84ee27a2d8471db94a47e140c171d573ed1c197
Desean <deseansyc@gmail.com>
2024-07-02 14:00:34 +0800

    Testing imports

--- test/unit/test_example.py
+++ test/unit/test_example.py
@@ -1,5 +1,9 @@
-import src.eventActionTriggers as EAT
-import src.eventActionTriggerConstants as EATC
+import sys
+
+import eventActionTriggers as EAT
+import eventActionTriggerConstants as EATC
+
+sys.path.insert(1, '/home/unicon/vms-ac-pi/src')
 
 def test_event_action_triggers_queue_output():
     event = EATC.create_event(EATC.AUTHENTICATED_SCAN, 1)
