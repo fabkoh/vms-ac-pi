ea03ddfebc6e37dfbe58c39e187a1ba316509ea3
Desean <deseansyc@gmail.com>
2024-07-02 17:37:30 +0800

    check_datetime tests

--- test/unit/test_eventActionTriggers.py
+++ test/unit/test_eventActionTriggers.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import datetime
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__)) # /vms-ac-pi/test/unit
 SRC_DIR = os.path.abspath(os.path.join(os.path.join(TEST_DIR, os.pardir), os.pardir)) # /vms-ac-pi
@@ -29,3 +30,42 @@ def test_update_event_action_triggers():
     original_json = open(SRC_DIR + "/src/json/eventActionTriggers.json")
     EAT.update_event_action_triggers()
     assert EAT.EVENT_ACTION_TRIGGERS_DATA == json.load(original_json)
+
+def test_check_datetime():
+    '''
+    This test creates a mock of what will be passed into the check_datetime function
+    and uses 'today', meaning the function should return true since the schedule
+    says that the current date is within schedule
+    '''
+    today_string_formatted = datetime.date.today().strftime("%Y-%m-%d")
+
+    mock_json_contents = {
+        today_string_formatted: [
+            {
+                "endtime": "24:00",
+                "starttime": "00:00"
+            }
+        ]
+    }
+
+    assert EAT.check_datetime(mock_json_contents)
+
+def test_not_check_datetime():
+    '''
+    This test creates a mock of what will be passed into the check_datetime function
+    and uses 'tomorrow', meaning the function should return false since the schedule
+    says that the current date is not within schedule
+    '''
+    tomorrow_datetime = datetime.date.today() + datetime.timedelta(days=1)
+    tomorrow_string_formatted = tomorrow_datetime.strftime("%Y-%m-%d")
+
+    mock_json_contents = {
+        tomorrow_string_formatted: [
+            {
+                "endtime": "24:00",
+                "starttime": "00:00"
+            }
+        ]
+    }
+
+    assert not EAT.check_datetime(mock_json_contents)
