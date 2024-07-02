bd5ccb64f2214f9a189547717998fa1d911edbfa
Desean <deseansyc@gmail.com>
2024-07-02 16:20:32 +0800

    Updating with failing test to check working

--- test/unit/test_example.py
+++ test/unit/test_example.py
@@ -13,3 +13,9 @@ def test_event_action_triggers_queue_output():
     EAT.queue_output(event)
 
     assert len(EAT.output_events) == 1
+
+def test_fail_event_action_triggers_queue_output():
+    event = EATC.create_event(EATC.AUTHENTICATED_SCAN, 1)
+    EAT.queue_output(event)
+
+    assert len(EAT.output_events) == 0
