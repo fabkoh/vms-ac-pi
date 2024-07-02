31871c55b0976fc1384e824d9ee4fdf888b27c53
Desean <deseansyc@gmail.com>
2024-07-02 16:59:00 +0800

    Test initiator, starting UT for eventActionTriggers

---
+++ src/test_src_initiator.py
@@ -0,0 +1,5 @@
+# This file helps pytest recognise the src folder for initialising of
+# tests in the test folder (vms-ac-pi/test/unit or vms-ac-pi/test/integration)
+
+def test_nothing():
+    pass
--- test/unit/test_eventActionTriggers.py
+++ test/unit/test_eventActionTriggers.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__)) # /vms-ac-pi/test/unit
 SRC_DIR = os.path.abspath(os.path.join(os.path.join(TEST_DIR, os.pardir), os.pardir)) # /vms-ac-pi
@@ -8,7 +9,7 @@ sys.path.insert(0, SRC_DIR)
 from src import eventActionTriggers as EAT
 from src import eventActionTriggerConstants as EATC
 
-def test_event_action_triggers_queue_output():
+def test_queue_output():
     event = EATC.create_event(EATC.AUTHENTICATED_SCAN, 1)
     EAT.queue_output(event)
 
@@ -16,10 +17,7 @@ def test_event_action_triggers_queue_output():
 
     EAT.output_events = []
 
-def test_fail_event_action_triggers_queue_output():
-    event = EATC.create_event(EATC.AUTHENTICATED_SCAN, 1)
-    EAT.queue_output(event)
-
-    assert len(EAT.output_events) == 0
-
-    EAT.output_events = []
+def test_update_event_action_triggers():
+    original_json = open(SRC_DIR + "/src/json/eventActionTriggers.json")
+    EAT.update_event_action_triggers()
+    assert EAT.EVENT_ACTION_TRIGGERS_DATA == json.load(original_json)
