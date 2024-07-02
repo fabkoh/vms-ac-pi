984c39413374477d8f9d93686e7bccb4f42367e9
Desean <deseansyc@gmail.com>
2024-07-02 14:23:32 +0800

    Editing relative path

--- src/test/unit/test_example.py
+++ src/test/unit/test_example.py
@@ -1,5 +1,5 @@
-import eventActionTriggers as EAT
-import eventActionTriggerConstants as EATC
+from .. import eventActionTriggers as EAT
+from .. import eventActionTriggerConstants as EATC
 
 def test_event_action_triggers_queue_output():
     event = EATC.create_event(EATC.AUTHENTICATED_SCAN, 1)
