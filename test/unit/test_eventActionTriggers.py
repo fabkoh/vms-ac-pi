3c95d09b3568d71c1790c056d327ba4456166e54
Desean <deseansyc@gmail.com>
2024-07-03 10:11:47 +0800

    get_entrance_from_ev_mngment tests

--- test/unit/test_eventActionTriggers.py
+++ test/unit/test_eventActionTriggers.py
@@ -10,6 +10,8 @@ sys.path.insert(0, SRC_DIR)
 from src import eventActionTriggers as EAT
 from src import eventActionTriggerConstants as EATC
 
+'''TODO: flush_output is hard to test, skip first and continue from get entrance from event management'''
+
 def test_queue_output():
     '''
     This test ensures that queue_output is actually putting the outputs to 
@@ -35,7 +37,7 @@ def test_check_datetime():
     '''
     This test creates a mock of what will be passed into the check_datetime function
     and uses 'today', meaning the function should return true since the schedule
-    says that the current date is within schedule
+    says that the current date is within schedule.
     '''
     today_string_formatted = datetime.date.today().strftime("%Y-%m-%d")
 
@@ -54,7 +56,7 @@ def test_not_check_datetime():
     '''
     This test creates a mock of what will be passed into the check_datetime function
     and uses 'tomorrow', meaning the function should return false since the schedule
-    says that the current date is not within schedule
+    says that the current date is not within schedule.
     '''
     tomorrow_datetime = datetime.date.today() + datetime.timedelta(days=1)
     tomorrow_string_formatted = tomorrow_datetime.strftime("%Y-%m-%d")
@@ -69,3 +71,41 @@ def test_not_check_datetime():
     }
 
     assert not EAT.check_datetime(mock_json_contents)
+
+def test_get_entrance_from_event_management():
+    '''
+    This test creates a mock of what will be passed into the get_entrance function,
+    with a simulated entrance ID, then verifies that the ID returned by the function
+    matches.
+    '''
+    mock_json_contents = {
+        "entrance": {
+            "entranceId": 1
+        },
+    }
+
+    assert EAT.get_entrance_from_event_management(mock_json_contents) == 1
+
+def test_not_get_entrance_from_event_management():
+    '''
+    This test creates a mock of what will be passed into the get_entrance function,
+    with a simulated entrance ID, then verifies that the ID returned by the function
+    will not match when it is not supposed to match.
+    '''
+    mock_json_contents = {
+        "entrance": {
+            "entranceId": 2
+        },
+    }
+
+    assert not EAT.get_entrance_from_event_management(mock_json_contents) == 1
+
+def test_get_both_entrance_from_event_management():
+    '''
+    This test creates a mock of what will be passed into the get_entrance function,
+    which in this case is nothing, and verifies that the function is supposed to
+    return the same as the BOTH_ENTRANCE constant in EAT
+    '''
+    mock_json_contents = {}
+
+    assert EAT.get_entrance_from_event_management(mock_json_contents) == EAT.BOTH_ENTRANCE
