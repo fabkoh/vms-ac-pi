df1e17b46712ef0b2b5da4eda3d0d30ebdf173d3
Desean <deseansyc@gmail.com>
2024-07-08 10:43:44 +0800

    Creating test for timer

---
+++ test/unit/test_timer.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+'''
+This part is needed to be able to import the files from src for testing
+'''
+TEST_DIR = os.path.dirname(os.path.abspath(__file__)) # /vms-ac-pi/test/unit
+SRC_DIR = os.path.abspath(os.path.join(os.path.join(TEST_DIR, os.pardir), os.pardir)) # /vms-ac-pi
+sys.path.insert(0, SRC_DIR)
+
+from src import timer
+
+def test_status():
+    '''
+    This test tests that status() returns true when the timer is still running
+    '''
+    _timer = timer()
+    _timer.start()
+    assert _timer.status()
+
+def test_not_status():
+    '''
+    This test tests that status() returns false when the timer has stopped 
+    running
+    '''
+    _timer = timer()
+    _timer.start()
+    _timer.stop()
+    assert not _timer.status()
