72342904cab1899a14a79654c9120fad8c83ae01
Desean <deseansyc@gmail.com>
2024-07-08 14:49:54 +0800

    Experiment mocking writelines 6

--- test/unit/test_changeStatic.py
+++ test/unit/test_changeStatic.py
@@ -14,13 +14,16 @@ import io # needed to mock writelines
 
 def test_change_static_ip(monkeypatch: pytest.MonkeyPatch):
     data_extracted = []
-    def mock_writelines(data):
+    def mock_writelines(self, data):
         nonlocal data_extracted
         data_extracted = data
     def mock_restart_eth0():
         pass
-    monkeypatch.setattr(_io.TextIOWrapper, "writelines", mock_writelines)
+    # monkeypatch.setattr(_io.TextIOWrapper, "writelines", mock_writelines)
     monkeypatch.setattr(changeStatic, "restart_eth0", mock_restart_eth0)
+
+    monkeypatch.setattr("builtins.open", lambda *args, **kwargs: mock_file)
+    mock_file = type("MockFile", (object,), {"writelines": mock_writelines, "__enter__": lambda s: s, "__exit__": lambda s, t, v, tb: None})()
 
     changeStatic.change_static_ip('test_ip_address.250', 'test_router198', 'test_dns8888')
 
