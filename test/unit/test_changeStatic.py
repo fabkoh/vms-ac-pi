07fd72e48ba773c1c4e5608f71293ddbaeb7ea96
Desean <deseansyc@gmail.com>
2024-07-08 15:04:49 +0800

    Experiment mocking writelines 7

--- test/unit/test_changeStatic.py
+++ test/unit/test_changeStatic.py
@@ -13,20 +13,52 @@ from src import changeStatic
 import io # needed to mock writelines
 
 def test_change_static_ip(monkeypatch: pytest.MonkeyPatch):
-    data_extracted = []
-    def mock_writelines(self, data):
-        nonlocal data_extracted
-        data_extracted = data
-    def mock_restart_eth0():
-        pass
-    # monkeypatch.setattr(_io.TextIOWrapper, "writelines", mock_writelines)
-    monkeypatch.setattr(changeStatic, "restart_eth0", mock_restart_eth0)
+    data_written = []
 
-    monkeypatch.setattr("builtins.open", lambda *args, **kwargs: mock_file)
-    mock_file = type("MockFile", (object,), {"writelines": mock_writelines, "__enter__": lambda s: s, "__exit__": lambda s, t, v, tb: None})()
+    def mock_readlines(self):
+        return ["interface eth0",
+                "static ip line",
+                "static routers line",
+                "static dns line"]
+    
+    def mock_writelines(self, data):
+        data_written = data
+    
+    class MockFile:
+        def __init__(self, mode):
+            self.mode = mode
+        
+        def writelines(self, data):
+            return mock_writelines(self, data)
+        
+        def readlines(self):
+            return mock_readlines(self)
+        
+        def __enter__(self):
+            return self
+        
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            pass
+
+    def mock_open(filepath, mode):
+        return MockFile(mode)
+    
+    monkeypatch.setattr("builtins.open", mock_open)
 
     changeStatic.change_static_ip('test_ip_address.250', 'test_router198', 'test_dns8888')
 
-    print(data_extracted)
+    print(data_written)
+    # data_extracted = []
+    # def mock_writelines(self, data):
+    #     nonlocal data_extracted
+    #     data_extracted = data
+    # def mock_restart_eth0():
+    #     pass
+    # monkeypatch.setattr(_io.TextIOWrapper, "writelines", mock_writelines)
+    # monkeypatch.setattr(changeStatic, "restart_eth0", mock_restart_eth0)
+
+    # changeStatic.change_static_ip('test_ip_address.250', 'test_router198', 'test_dns8888')
+
+    # print(data_extracted)
 
 
