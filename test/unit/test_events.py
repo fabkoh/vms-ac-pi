39e38f19b9d121794f3f96824abf0272a52479e1
Desean <deseansyc@gmail.com>
2024-07-09 13:29:51 +0800

    Creat test_events

---
+++ test/unit/test_events.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import datetime
+
+'''
+This part is needed to be able to import the files from src for testing
+'''
+TEST_DIR = os.path.dirname(os.path.abspath(__file__)) # /vms-ac-pi/test/unit
+SRC_DIR = os.path.abspath(os.path.join(os.path.join(TEST_DIR, os.pardir), os.pardir)) # /vms-ac-pi
+sys.path.insert(0, SRC_DIR)
+
+from src import events
+
+def test_verify_datetime_day():
+    '''
+    This test tests that verify_datetime correctly returns true when the date is
+    today
+    '''
+    today_string_formatted = datetime.date.today().strftime("%Y-%m-%d")
+
+    mock_json_contents = {
+        today_string_formatted: [
+            {
+                "endtime": "24:00",
+                "starttime": "00:00"
+            }
+        ]
+    }
+
+    assert events.verify_datetime(mock_json_contents)
+
+def test_not_verify_datetime_day():
+    '''
+    This test tests that verify_datetime correctly returns false when the date 
+    is tomorrow
+    '''
+    tomorrow_datetime = datetime.date.today() + datetime.timedelta(days=1)
+    tomorrow_string_formatted = tomorrow_datetime.strftime("%Y-%m-%d")
+
+    mock_json_contents = {
+        tomorrow_string_formatted: [
+            {
+                "endtime": "24:00",
+                "starttime": "00:00"
+            }
+        ]
+    }
+
+    assert not events.verify_datetime(mock_json_contents)
+
+def test_verify_datetime_time():
+    today_string_formatted = datetime.date.today().strftime("%Y-%m-%d")
+    print(datetime.datetime.now())
