d3ab4133287d9606efb5e5b646681992bb594adb
junnengsoo <soojunneng01@gmail.com>
2024-07-10 15:17:27 +0800

    Test short timings for buzzer

--- src/GPIOconfig.py
+++ src/GPIOconfig.py
@@ -342,10 +342,10 @@ def test_buzzer():
       # GPIOconfig.activate_led(eventActionTriggerConstants.BOTH_ENTRANCE, 0.25)
       print("test", 1)
       # pi.write(18, 1)
-      pi.write(4, 1)
+      pi.write(17, 1)
       time.sleep(1)
       # pi.write(18, 0)
-      pi.write(4, 0)
+      pi.write(17, 0)
       time.sleep(1)
 
 # def activate_buzz(entrance,t):
