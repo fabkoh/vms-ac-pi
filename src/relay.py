08dba14bb49ade1b87afc7bed855aa2fca924019
junnengsoo <soojunneng01@gmail.com>
2024-07-10 14:54:58 +0800

    Test short timings for buzzer

--- src/relay.py
+++ src/relay.py
@@ -111,9 +111,9 @@ def test_buzzer(gpio, level, tick):
     print("testing buzzer")
     pi.set_mode(gpio, pigpio.OUTPUT)
     for i in range(0, 5):
-        GPIOconfig.activate_buzz(eventActionTriggerConstants.BOTH_ENTRANCE, 1)
-        # GPIOconfig.activate_led(eventActionTriggerConstants.BOTH_ENTRANCE, 1)
-        time.sleep(2)
+        # GPIOconfig.activate_buzz(eventActionTriggerConstants.BOTH_ENTRANCE, 1)
+        GPIOconfig.activate_led(eventActionTriggerConstants.BOTH_ENTRANCE, 0.25)
+        time.sleep(0.5)
         # pi.write(gpio, 1)
         # time.sleep(1)
         # pi.write(gpio, 0)
