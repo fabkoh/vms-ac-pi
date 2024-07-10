5e03b602be813b3b1a553937793aa27a4dd98ac0
junnengsoo <soojunneng01@gmail.com>
2024-07-10 14:04:46 +0800

    Put test_buzzer into threadpool

--- src/relay.py
+++ src/relay.py
@@ -245,8 +245,8 @@ def trigger_relay_one(thirdPartyOption=None):
         setupRelayPin(outputPin)
         print("opening")
         logger.info("Before toggleRelay1")
-        # toggleRelay1(outputPin, 'High', 5000, 1000, 1)
         thread_pool_executor.submit(toggleRelay1, outputPin, 'High', 5000, 1000, 1)
+        thread_pool_executor.submit(test_buzzer, 23, 'High', 1)
 
         # cleanupGpio()
     except RuntimeError:
