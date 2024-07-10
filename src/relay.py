a70417a8619658262b957d32a6b3017e01cd81e1
junnengsoo <soojunneng01@gmail.com>
2024-07-10 15:08:28 +0800

    Test short timings for buzzer

--- src/relay.py
+++ src/relay.py
@@ -240,7 +240,6 @@ def trigger_relay_one(thirdPartyOption=None):
         print("opening")
         logger.info("Before toggleRelay1")
         thread_pool_executor.submit(toggleRelay1, outputPin, 'High', 5000, 1000, 1)
-        thread_pool_executor.submit(test_buzzer, 23, 'High', 1)
 
         # cleanupGpio()
     except RuntimeError:
