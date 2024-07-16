84b76d530839a35479fc38859eb9e6b6dff93ec2
Desean <deseansyc@gmail.com>
2024-07-16 10:52:31 +0800

    Test pause before submit to threadpool

--- src/events.py
+++ src/events.py
@@ -415,6 +415,7 @@ def led_and_buzzer_correct_cred(entrance_id):
         # thread_pool_executor.submit(thread_pool_buzz(GPIOconfig.E1_OUT_Buzz))
         # thread_pool_executor.submit(thread_pool_led(GPIOconfig.E1_OUT_Led))
         thread_pool_executor.submit(thread_pool_buzz(4)) # Buzzer pin
+        time.sleep(0.5)
         thread_pool_executor.submit(thread_pool_led(17)) # LED pin
     elif entrance_id == E2:
         thread_pool_executor.submit(thread_pool_buzz(GPIOconfig.E2_OUT_Buzz))
