af6fc27c77bf267165392b9fea0a595b8ba7884b
Desean <deseansyc@gmail.com>
2024-07-16 14:27:26 +0800

    Test OUT before IN

--- src/events.py
+++ src/events.py
@@ -408,14 +408,14 @@ def led_and_buzzer_correct_cred(entrance_id):
     
     if entrance_id == E1:
         thread_pool_executor.submit(
-            thread_pool_buzz_led(GPIOconfig.E1_IN_Buzz, GPIOconfig.E1_IN_Led))
-        thread_pool_executor.submit(
             thread_pool_buzz_led(GPIOconfig.E1_OUT_Buzz, GPIOconfig.E1_OUT_Led))
+        thread_pool_executor.submit(
+            thread_pool_buzz_led(GPIOconfig.E1_IN_Buzz, GPIOconfig.E1_IN_Led))
     elif entrance_id == E2:
         thread_pool_executor.submit(
-            thread_pool_buzz_led(GPIOconfig.E2_IN_Buzz, GPIOconfig.E2_IN_Led))
-        thread_pool_executor.submit(
             thread_pool_buzz_led(GPIOconfig.E2_OUT_Buzz, GPIOconfig.E2_OUT_Led))
+        thread_pool_executor.submit(
+            thread_pool_buzz_led(GPIOconfig.E2_IN_Buzz, GPIOconfig.E2_IN_Led))
 
 def led_and_buzzer_wrong_cred(entrance_id):
     '''
@@ -438,11 +438,11 @@ def led_and_buzzer_wrong_cred(entrance_id):
             time.sleep(0.1)
     
     if entrance_id == E1:
-        thread_pool_executor.submit(thread_pool_buzz(GPIOconfig.E1_IN_Buzz))
         thread_pool_executor.submit(thread_pool_buzz(GPIOconfig.E1_OUT_Buzz))
+        thread_pool_executor.submit(thread_pool_buzz(GPIOconfig.E1_IN_Buzz))
     elif entrance_id == E2:
-        thread_pool_executor.submit(thread_pool_buzz(GPIOconfig.E2_IN_Buzz))
         thread_pool_executor.submit(thread_pool_buzz(GPIOconfig.E2_OUT_Buzz))
+        thread_pool_executor.submit(thread_pool_buzz(GPIOconfig.E2_IN_Buzz))
 
 def reader_detects_bits(bits, value, entrance):
 
