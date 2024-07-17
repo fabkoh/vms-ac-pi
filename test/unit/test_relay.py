f433088884e73652ae2170b75ff2ff1b257bc94f
Desean <deseansyc@gmail.com>
2024-07-17 16:36:37 +0800

    Increase wait time

--- test/unit/test_relay.py
+++ test/unit/test_relay.py
@@ -159,7 +159,7 @@ def test_trigger_relay_one(mock_relaySetHighLow, mock_setup_cleanup):
     # ------------- TEST SECTION: Trigger Relay 1, no TPO ----------------------
     relay.trigger_relay_one()
 
-    time.sleep(5) # To allow threadpoolexecutor to finish task
+    time.sleep(10) # To allow threadpoolexecutor to finish task
 
     assert relayPinSetting == ["High", "Low"]
     assert relayPinNumber == 51
@@ -171,7 +171,7 @@ def test_trigger_relay_one(mock_relaySetHighLow, mock_setup_cleanup):
     # ------------- TEST SECTION: Trigger Relay 1, TPO Gen 1 -------------------
     relay.trigger_relay_one("GEN_OUT_1")
 
-    time.sleep(5) # To allow threadpoolexecutor to finish task
+    time.sleep(10) # To allow threadpoolexecutor to finish task
 
     assert relayPinSetting == ["High", "Low"]
     assert relayPinNumber == 52
