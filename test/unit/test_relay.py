de62678a3f9f7ec6b3fea4269dc7ff43854ba4b6
Desean <deseansyc@gmail.com>
2024-07-17 16:28:09 +0800

    Test trigger_relay_one

--- test/unit/test_relay.py
+++ test/unit/test_relay.py
@@ -17,6 +17,11 @@ relayPinSetting = []
 
 @pytest.fixture
 def mock_setup_cleanup(monkeypatch: pytest.MonkeyPatch):
+    '''
+    This is a fixture to mock setGpioMode, setupRelayPin and cleanupGpio, so
+    that the GPIO module doesn't actually carry out the setup and cleanup
+    operations, as we don't need them.
+    '''
     def mock_setGpioMode():
         pass
     
@@ -32,6 +37,10 @@ def mock_setup_cleanup(monkeypatch: pytest.MonkeyPatch):
 
 @pytest.fixture
 def mock_relaySetHighLow(monkeypatch: pytest.MonkeyPatch):
+    '''
+    This is a fixture to mock setRelayPinHigh and setRelayPinLow, so that
+    the relays IRL don't actually get triggered during testing.
+    '''
     def mock_setRelayPinHigh(relayPin):
         global relayPinSetting, relayPinNumber
         relayPinSetting.append("High")
@@ -46,6 +55,10 @@ def mock_relaySetHighLow(monkeypatch: pytest.MonkeyPatch):
     monkeypatch.setattr(relay, "setRelayPinLow", mock_setRelayPinLow)
 
 def test_setRelay(mock_relaySetHighLow):
+    '''
+    This function tests that setRelay properly sets the relay pin to high or low
+    depending on the inputs.
+    '''
     global relayPinNumber, relayPinSetting
     relayPinNumber = 0
     relayPinSetting = []
@@ -69,8 +82,11 @@ def test_setRelay(mock_relaySetHighLow):
     relayPinSetting = []
 
 def test_toggleRelay1(mock_relaySetHighLow, mock_setup_cleanup):
+    '''
+    This functions tests that toggleRelay1 properly toggles the relay pin high,
+    and low, depending on the toggle count.
+    '''
     global relayPinNumber, relayPinSetting
-
     relayPinNumber = 0
     relayPinSetting = []
 
@@ -93,8 +109,11 @@ def test_toggleRelay1(mock_relaySetHighLow, mock_setup_cleanup):
     relayPinSetting = []
 
 def test_toggleRelayGen(mock_relaySetHighLow, mock_setup_cleanup):
+    '''
+    This function tests that toggleRelayGen properly toggles the correct gen
+    pin high and low once, based on the inputs
+    '''
     global relayPinNumber, relayPinSetting
-
     relayPinNumber = 0
     relayPinSetting = []
 
@@ -115,3 +134,50 @@ def test_toggleRelayGen(mock_relaySetHighLow, mock_setup_cleanup):
 
     relayPinNumber = 0
     relayPinSetting = []
+
+def test_trigger_relay_one(mock_relaySetHighLow, mock_setup_cleanup):
+    '''
+    This test tests that trigger_relay_one is able to trigger the correct relay,
+    be it the default or the general option, based on the input.
+    '''
+    global relayPinNumber, relayPinSetting
+    relayPinNumber = 0
+    relayPinSetting = []
+
+    # To remember original value to set back later
+    original_Relay_1 = relay.Relay_1
+    original_GEN_OUT_1 = relay.GEN_OUT_1
+    original_GEN_OUT_2 = relay.GEN_OUT_2
+    original_GEN_OUT_3 = relay.GEN_OUT_3
+
+    # setting Relay_1 to testing value
+    relay.Relay_1 = 51
+    relay.GEN_OUT_1 = 52
+    relay.GEN_OUT_2 = 53
+    relay.GEN_OUT_3 = 54
+
+    # ------------- TEST SECTION: Trigger Relay 1, no TPO ----------------------
+    relay.trigger_relay_one()
+
+    assert relayPinSetting == ["High", "Low"]
+    assert relayPinNumber == 51
+    # ------------- END OF TEST SECTION ----------------------------------------
+
+    relayPinNumber = 0
+    relayPinSetting = []
+
+    # ------------- TEST SECTION: Trigger Relay 1, TPO Gen 1 -------------------
+    relay.trigger_relay_one("GEN_OUT_1")
+
+    assert relayPinSetting == ["High", "Low"]
+    assert relayPinNumber == 52
+    # ------------- END OF TEST SECTION ----------------------------------------
+
+    relayPinNumber = 0
+    relayPinSetting = []
+
+    # Setting back original functions
+    relay.Relay_1 = original_Relay_1
+    relay.GEN_OUT_1 = original_GEN_OUT_1
+    relay.GEN_OUT_2 = original_GEN_OUT_2
+    relay.GEN_OUT_3 = original_GEN_OUT_3
