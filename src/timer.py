2e018b10973c30ae157a4e7af4dee17369fecb55
yongning0310 <85162884+yongning0310@users.noreply.github.com>
2024-07-22 18:05:02 +0800

    remove logging scripts

--- src/timer.py
+++ src/timer.py
@@ -10,7 +10,7 @@ class Timer:
     def start(self):
         """Start a new timer"""
         if self._start_time is not None:
-            print("Timer is running. Use .stop() to stop it")
+            # print("Timer is running. Use .stop() to stop it")
             return 
             raise TimerException(f"Timer is running. Use .stop() to stop it")
 
@@ -19,18 +19,18 @@ class Timer:
     def stop(self):
         """Stop the timer, and report the elapsed time"""
         if self._start_time is None:
-            print("Timer is not running. Use .start() to start it")
+            # print("Timer is not running. Use .start() to start it")
             return 
             raise TimerException(f"Timer is not running. Use .start() to start it")
             
         elapsed_time = time.perf_counter() - self._start_time
         self._start_time = None
-        print(f"Elapsed time: {elapsed_time:0.4f} seconds")
+        # print(f"Elapsed time: {elapsed_time:0.4f} seconds")
     
     def check(self,TIME):
         """return True if current_elapsed_time exceeds TIME"""
         if self._start_time is None:
-            print("Timer is not running. Use .start() to start it")
+            # print("Timer is not running. Use .start() to start it")
             return 
             
         current_elapsed_time = time.perf_counter() - self._start_time
