ca3320f56e6cc6007f8cbf3eed48f9c6e7af8320
yongning0310 <85162884+yongning0310@users.noreply.github.com>
2024-07-22 16:52:31 +0800

    multi-process

--- src/relay.py
+++ src/relay.py
@@ -237,10 +237,10 @@ def trigger_relay_one(thirdPartyOption=None):
         # toggleRelay1(outputPin, 'High', 5000, 1000, 1)
         print("before creating thread: ", datetime.now())
         thread_pool_executor.submit(toggleRelay1, outputPin, 'High', 5000, 1000, 1, priority=1)
-        # toggleRelay1(relayPin=outputPin, activateLevel='High',
-        #              activateMilliSeconds=5000, deActivateMilliSeconds=1000,
-        #              toggleCount=1)
-        # cleanupGpio()
+        toggleRelay1(relayPin=outputPin, activateLevel='High',
+                     activateMilliSeconds=5000, deActivateMilliSeconds=1000,
+                     toggleCount=1)
+        cleanupGpio()
 
         # cleanupGpio()
     except RuntimeError:
