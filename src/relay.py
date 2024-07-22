78a244929e8c13fb79e31204148b0a9cf800d4d5
Desean <deseansyc@gmail.com>
2024-07-22 11:44:53 +0800

    Test without thread pool executor

--- src/relay.py
+++ src/relay.py
@@ -239,7 +239,8 @@ def trigger_relay_one(thirdPartyOption=None):
         setupRelayPin(outputPin)
         print("opening")
         logger.info("Before toggleRelay1")
-        thread_pool_executor.submit(toggleRelay1, outputPin, 'High', 5000, 1000, 1)
+        # thread_pool_executor.submit(toggleRelay1, outputPin, 'High', 5000, 1000, 1)
+        toggleRelay1(outputPin, 'High', 5000, 1000, 1)
 
         # cleanupGpio()
     except RuntimeError:
