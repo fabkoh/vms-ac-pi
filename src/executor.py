04d2e4530ae861e3ea286f7e2f86011ca710ffcb
yongning0310 <85162884+yongning0310@users.noreply.github.com>
2024-07-22 16:21:11 +0800

    multi-process

--- src/executor.py
+++ src/executor.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import gc
 import os
 import time
@@ -49,6 +50,7 @@ class ThreadPoolMonitor:
             task_id = self.task_id_counter
             self.task_id_counter += 1
             self.active_tasks[task_id] = func.__name__  # Track task by name
+        print(task_id, "creating thread: ", datetime.now())
         logger.info(f"Task {task_id} submitted: {func.__name__}. Total submitted: {len(self.active_tasks)}")
         future = self.executor.submit(self._run, task_id, func, *args, **kwargs)
         future.add_done_callback(lambda f: self._task_complete(task_id, f))
@@ -61,6 +63,7 @@ class ThreadPoolMonitor:
         return result
 
     def _task_complete(self, task_id, future):
+        print(task_id, " thread completed: ", datetime.now())
         with self.lock:
             if task_id in self.active_tasks:
                 del self.active_tasks[task_id]
