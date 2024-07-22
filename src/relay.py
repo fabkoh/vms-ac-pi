a435e91919f283fa07b18856c60f363fb6c5d786
Desean <deseansyc@gmail.com>
2024-07-22 09:38:59 +0800

    Print time to find out where delay

--- src/relay.py
+++ src/relay.py
@@ -92,7 +92,7 @@ def setRelayPinLow(relayPin):
 
 
 def activateRelay(relayPin, activateLevel):
-    print("activateRelay", activateLevel, relayPin)
+    print("activateRelay", activateLevel, relayPin, time.time())
     if activateLevel == 'High':
         setRelayPinHigh(relayPin)
     else:
