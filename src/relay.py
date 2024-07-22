d1079cee11c167e43719e3631b67c9427757d01c
yongning0310 <85162884+yongning0310@users.noreply.github.com>
2024-07-22 18:05:54 +0800

    change relay two to thread pool

--- src/relay.py
+++ src/relay.py
@@ -258,10 +258,12 @@ def trigger_relay_two(thirdPartyOption=None):
     try:
         setGpioMode()
         setupRelayPin(outputPin)
-        toggleRelay2(relayPin=outputPin, activateLevel='High',
-                     activateMilliSeconds=5000, deActivateMilliSeconds=1000,
-                     toggleCount=1)
-        cleanupGpio()
+        thread_pool_executor.submit(toggleRelay2, outputPin, 'High', 5000, 1000, 1)
+
+        # toggleRelay2(relayPin=outputPin, activateLevel='High',
+        #              activateMilliSeconds=5000, deActivateMilliSeconds=1000,
+        #              toggleCount=1)
+        # cleanupGpio()
     except RuntimeError:
         print("Entrance is still opened")
     return
