d8c583ac055b0b773570de8ba1eb0c775b74cfa8
Desean <deseansyc@gmail.com>
2024-07-22 11:46:29 +0800

    Undo testing change

--- src/relay.py
+++ src/relay.py
@@ -239,8 +239,7 @@ def trigger_relay_one(thirdPartyOption=None):
         setupRelayPin(outputPin)
         print("opening")
         logger.info("Before toggleRelay1")
-        # thread_pool_executor.submit(toggleRelay1, outputPin, 'High', 5000, 1000, 1)
-        toggleRelay1(outputPin, 'High', 5000, 1000, 1)
+        thread_pool_executor.submit(toggleRelay1, outputPin, 'High', 5000, 1000, 1)
 
         # cleanupGpio()
     except RuntimeError:
