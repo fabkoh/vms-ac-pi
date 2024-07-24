57727cd9372acf6435f9dfce50cdca22a7b504a6
Desean <deseansyc@gmail.com>
2024-07-24 17:19:38 +0800

    Add extra mock for update logs and server

--- test/unit/test_eventsMod.py
+++ test/unit/test_eventsMod.py
@@ -58,7 +58,9 @@ def mock_all_relevant_functions(monkeypatch: pytest.MonkeyPatch):
     monkeypatch.setattr("logging.getLogger", mock_getLogger)
     monkeypatch.setattr("eventActionTriggers.event_trigger_cb", 
                         mock_event_trigger_cb)
-    monkeypatch.setattr("eventsMod.update_logs_and_server", 
+    monkeypatch.setattr("update_logs_and_server", 
+                        mock_update_logs_and_server)
+    monkeypatch.setattr("eventsMod.update_logs_and_server",
                         mock_update_logs_and_server)
 
 def test_record_auth_scans(mock_all_relevant_functions):
