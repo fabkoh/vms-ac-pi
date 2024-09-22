09e96e6b8fcb03e92c7b87d5913651ea1aaa5592
Desean <deseansyc@gmail.com>
2024-09-22 11:17:20 +0800

    Add print separator

--- src/events.py
+++ src/events.py
@@ -556,6 +556,7 @@ def reader_detects_bits(bits, value, entrance):
     # print("bits={} value={}".format(bits, value))
 
     # print(bits, value)
+    print("-----------------------------------------------------------") # TODO: REMOVE
     print("bits before checks={} value before edits={}".format(bits, value)) # TODO: REMOVE
     
     credential_added = False
